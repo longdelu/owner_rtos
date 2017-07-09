e65770eac26a58d1afa029073b1b963277bbfe40
longguanrong <861086182@qq.com>
2017-07-09 22:57:04 +0800

    提交第2课时

--- usr/rtos_task.h
+++ usr/rtos_task.h
@@ -7,11 +7,60 @@ extern "C" {
 
 #include "c_lib.h"  
     
-typedef struct rtos_task {
+/* Cortex-M的堆栈单元类型：堆栈单元的大小为32位，所以使用uint32_t */
+typedef uint32_t taskstack_t;
     
-    uint32_t *task_stack;
+typedef struct rtos_task {
+   
+    /** \brief 堆栈栈顶地址 */    
+    uint32_t *task_stack_top;
+    
+    /** \brief 堆栈的起即地址 */
+    uint32_t * stack_base;
+
+    /** \brief 堆栈的总容量 */
+    uint32_t stack_size;
     
 }rtos_task_t;
+
+/**
+ * \brief  任务初始化函数
+ *
+ * \param[in] task: 任务结构体指针
+ * \param[in] task_entry: 任务入口函数
+ * \param[in] p_arg: 任务入口函数参数
+ * \param[in] task_prio: 任务的优先级
+ * \param[in] task_stack: 任务堆栈的指针
+ * \param[in] task_stack_size: 任务堆栈的大小,以字节为单位
+ *
+ * \return    无
+ */
+void rtos_task_init(rtos_task_t * task, 
+                    void (*task_entry) (void *p_arg), 
+                    void *p_arg, 
+                    uint32_t task_prio, 
+                    uint32_t *task_stack,
+                    uint32_t task_stack_size);
+                    
+                    
+/**
+ * \brief 切换到第一个任务运行，启动操作系统第一个任务
+ *                    
+ * \param[in] 无
+ *
+ * \return    无                                      
+ */
+void rtos_task_run_first (void);                    
+                    
+                    
+/**
+ * \brief 任务调度函数 
+ *                    
+ * \param[in] 无
+ *
+ * \return    无
+ */
+void rtos_task_sched(void);                    
 
 
 #ifdef __cplusplus
