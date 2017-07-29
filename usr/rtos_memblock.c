3663394687733a163b5d6f6911351235f9c34f28
longguanrong <861086182@qq.com>
2017-07-30 02:00:05 +0800

    加入存储块文件实现

---
+++ usr/rtos_memblock.c
@@ -0,0 +1,33 @@
+/*******************************************************************************
+*                                 Embedded_rtos
+*                       ----------------------------
+*                       innovating embedded platform
+*
+* Copyright (c) 2001-2017 Free soft.
+* All rights reserved.
+*
+* Contact information:
+* web site:    
+* e-mail:      
+*******************************************************************************/
+
+
+/**
+ * \file
+ * \brief RTOS ¥Ê¥¢øÈ µœ÷
+ * \internal
+ * \par Modification history
+ * - 1.00 17-07-10  nwt, first implementation.
+ * \endinternal
+ */
+ 
+ #include "rtos_memblock.h"
+ 
+ 
+ 
+ 
+ 
+ 
+/* end of file */
+
+ 
---
+++ usr/rtos_memblock.h
@@ -0,0 +1,27 @@
+/**
+ * \file
+ * \brief RTOS 消息邮箱头文件
+ * \internal
+ * \par Modification history
+ * - 1.00 17-07-10  nwt, first implementation.
+ * \endinternal
+ */
+
+
+#ifndef __RTOS_MEMBLOCK_H
+#define __RTOS_MEMBLOCK_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+    
+#include "c_lib.h"
+#include "rtos_task_event.h"
+    
+    
+    #ifdef __cplusplus
+}
+#endif
+
+#endif /* ___RTOS_MEMBLOCK_H */
+
